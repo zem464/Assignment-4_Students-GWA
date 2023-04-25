6bc5af67d08248dc264111dbd53ae3a2f03c345b
Iris Membrere <irismembrere@gmail.com>
2023-04-25 20:41:44 +0800

    Create and open the text file

---
+++ Assignment-4_Student-GWA_txt.py
@@ -0,0 +1,8 @@
+# Zemerelin Iris M. Membrere
+# BSCPE 1-4
+# Assignment 4
+
+# Problem 2 - The one with the highest GWA among 20 students
+
+# Create and open the file with students and gwa
+with open("students_gwa.txt") as student_file:
